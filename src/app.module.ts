0faa0da554360da3b64b335a6fd83466e2e37ada
musamarabbani <usama.rabbani@invozone.com>
2023-04-05 19:27:39 +0500

    create db connection and implement validation pipes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { typeOrmConfig } from './config/typeorm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TasksModule],
-  controllers: [],
-  providers: [],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule],
 })
 export class AppModule {}
